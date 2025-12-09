e6161a2ed2117b7d99ddbe490ed216b395473412
user_name <zetoyan.aram2@gmail.com>
2025-12-09 17:38:35 +0400

    my works

--- script as bmi_calculator.py
+++ script as bmi_calculator.py
@@ -1,6 +1,6 @@
-name = input(str) #different names
-age = input(int) #old
-weight = input(float) #kg
-height = input(float) #cm or m
-BMI = weight / (height * 2)
-print("BMI")
+# name = input(str) #different names
+# age = input(int) #old
+weight = float(input("qo tarberaky"))
+height = float(input(" qo tarberaky")) #cm or m
+BMI = weight/(height ** 2)
+print(" BMI ")
